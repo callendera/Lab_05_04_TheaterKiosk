796817a740e5bc0c224b1c3be16a839609b62cd7
callendera <acallender@live.sscc.edu>
2022-09-22 21:47:30 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//class MovieTheater
+public class Main {
+    //   main()
+    public static void main(String[] args) {
+
+
+        // num userAge
+        int userAge = 27;
+
+        // output “How old are you?: “
+        System.out.println("How old are you?: ");
+        // input userAge
+        System.out.println("Simulated User age: " + userAge);
+
+        // if userAge >= 21
+        if(userAge >= 21) {
+            // output “You get a paper wristband.”
+            System.out.println("You get a paper wristband");
+        }//endif
+
+    } //return
+} // end class
